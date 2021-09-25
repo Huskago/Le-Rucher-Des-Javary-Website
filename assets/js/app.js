5f7945185572e389fd5cb6af8c531947a33f35b0
tiago.ferreiralourenco <tiago.ferreiralourenco@monlycee.net>
2021-09-25 14:16:28 +0200

    init

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.clientY - 10) + "px; left: " + (e.clientX - 10) + "px;")
+    cursor.classList.remove("hidden");
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("cursorClick");
+
+    setTimeout(() => {
+        cursor.classList.remove("cursorClick");
+    }, 1250)
+})
