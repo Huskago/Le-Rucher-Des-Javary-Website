39a7a603a694b878dd71ceaefb16ed9fb1c236ab
Huskago <tiagoflfl0202@gmail.com>
2021-10-12 14:38:20 +0200

    Update design and update image

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,16 @@
+// Copyright Current Year
+
+var currentYear = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear;
+
+// Cursor
+
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
     cursor.setAttribute("style", "top: " + (e.clientY - 10) + "px; left: " + (e.clientX - 10) + "px;")
     cursor.classList.remove("hidden");
-})
+});
 
 document.addEventListener('click', () => {
     cursor.classList.add("cursorClick");
@@ -11,4 +18,4 @@ document.addEventListener('click', () => {
     setTimeout(() => {
         cursor.classList.remove("cursorClick");
     }, 1250)
-})
+});
